f9b476efa09d64f5849f77194c6e352dddfa88e1
syed zohaib <zohaibsyed914@gmail.com>
2024-06-14 15:56:04 +0500

    Game Done

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ let newturn = new Audio("./assets/changeturn.mp3")
 let turn = "X"
 let isgameover = false
 
+
+
+
 // function to change turn
 
 const changeTurn = () => {
@@ -18,14 +21,14 @@ const changeTurn = () => {
 const checkWin = () => {
     let boxText = document.getElementsByClassName('boxText')
     let wins = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
 
     ]
     wins.forEach(e => {
@@ -33,24 +36,33 @@ const checkWin = () => {
             document.querySelector('.info').innerText = boxText[e[0]].innerText + " Won"
             isgameover = true;
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px"
+            document.querySelector('.line').style.width = "20vw"
+            document.querySelector('.line').style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+
         }
     })
 }
 
 // game logic
-// music.play()
+
 let boxes = document.getElementsByClassName("box")
 Array.from(boxes).forEach(element => {
     let boxText = element.querySelector('.boxText')
     element.addEventListener('click', () => {
         if (boxText.innerText === '') {
             boxText.innerText = turn
+            music.play()
             turn = changeTurn();
             newturn.play()
             checkWin();
+
+
             if (!isgameover) {
                 document.getElementsByClassName("info")[0].innerText = "Turn for " + turn
+            } else {
+                gamewin.play()
             }
+
         }
     })
 })
@@ -64,6 +76,7 @@ reset.addEventListener('click', () => {
     })
     turn = "X"
     isgameover = false
+    document.querySelector(".line").style.width = "0px"
     document.getElementsByClassName("info")[0].innerText = "Turn for " + turn
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
 
